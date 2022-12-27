c0523ffcf47819147618281d22aebda4f8e0386d
RamanSlinka <romuch1986@gmail.com>
2022-12-27 18:27:42 +0100

    used method "gn generate component [name] / ng g c [name]"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {Test1Component} from "./test1/test1.component";
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component
+    Test1Component,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test3/test3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test3',
+  templateUrl: './test3.component.html',
+  styleUrls: ['./test3.component.css']
+})
+export class Test3Component {
+
+}
