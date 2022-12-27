2fccaeb35d2990e2a3f609fb31028c25184fa98e
RamanSlinka <romuch1986@gmail.com>
2022-12-27 16:04:38 +0100

    initial commit; created first Angular component :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Test1Component} from "./test1/test1.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.css']
+})
+export class Test1Component {
+
+}
