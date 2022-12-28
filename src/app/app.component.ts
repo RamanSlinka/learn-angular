748ec1676fcb141683fd15e730ce1c37608de091
RamanSlinka <romuch1986@gmail.com>
2022-12-28 23:18:49 +0100

    learn methods : interpolation, property binding, event binding, two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+
+interface IUser {
+  age: number
+  name: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learn-angular';
+  title = 'Learn-angular';
+
+  user: IUser= {
+    age: 25,
+    name: 'Valdemar'
+  };
+  isAppLoading = true
+  text = ''
+  text2 = 'start value'
+
+  constructor() {
+  setTimeout(()=> {
+    this.isAppLoading = false
+    }, 3000)
+  }
+
+  changeTitleHandler() {
+    this.title = 'Angular is amazing !'
+  }
+
+  changeTextHandler(event: Event) {
+    this.text = (event.currentTarget as HTMLInputElement).value
+  }
+
+  changeText2Handler(event: Event) {
+    this.text2 = (event.currentTarget as HTMLInputElement).value
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {Test1Component} from "./test1/test1.component";
-import { Test2Component } from './test2/test2.component';
-import { Test3Component } from './test3/test3.component';
-import { Test4Component } from './user/components/test4/test4.component';
-import { Test6Component } from './test6/test6.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    Test4Component,
-    Test6Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
