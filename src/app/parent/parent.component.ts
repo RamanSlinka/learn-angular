8481b4ca48ba145833b99fea47f4ea8eeebd2ffa
RamanSlinka <romuch1986@gmail.com>
2022-12-30 16:43:07 +0100

    learn directives : *ngFor

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -2,6 +2,18 @@ import {Component} from '@angular/core';
 import {Grade} from "./child/child.component";
 
 
+interface WeekGrade {
+  id: number
+  day: string
+  gridItem: number
+}
+interface Lessons {
+  id: number
+  title: string
+  weekGrades: WeekGrade[]
+}
+
+
 @Component({
   selector: 'ang-parent',
   templateUrl: './parent.component.html',
@@ -12,6 +24,30 @@ export class ParentComponent {
   english?: number
 
   grades: string[] = ['math: 5, physic: 6']
+
+
+
+  lessons: Lessons[] = [
+    {
+      id:0,
+      title: 'Math',
+      weekGrades: [
+        {id: 1,day: 'monday', gridItem: 7},
+        {id: 2,day: 'wednesday', gridItem: 5},
+        {id: 3, day: 'friday',gridItem: 9},
+      ]
+    }, {
+      id:1,
+      title: 'English',
+      weekGrades: [
+        {id: 1,day: 'tuesday', gridItem: 9},
+        {id: 2,day: 'wednesday', gridItem: 10},
+        {id: 3, day: 'thursday',gridItem: 8},
+      ]
+    }
+  ]
+
+
 
   getGrade(value: Grade) {
     this.math = value.math
