0b064fa2c2458b6c563ea6b52cd5592a27b9a2b6
RamanSlinka <romuch1986@gmail.com>
2022-12-31 16:56:00 +0100

    learn directives : ngStyle

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -10,6 +10,12 @@ export class ParentComponent {
   isSuccess = false
   error = false
 
+  styles = {
+    color: 'brown',
+    border: '2px green solid',
+    width: '250px'
+  }
+
   constructor() {
     setTimeout(() => {
       this.isSuccess = true
