8202dfe9f9d6bd2e4f27436096b20e8fe9f13ebe
RamanSlinka <romuch1986@gmail.com>
2023-01-14 13:42:32 +0100

    learn directives : worked with server: add error handling => refactored, used Bind(this)

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -28,6 +28,12 @@ export class TodosService {
   constructor(private http: HttpClient, private beautyLoggerService: BeautyLoggerService) {
   }
 
+  private errorHandler(err: HttpErrorResponse)  {
+  this.beautyLoggerService.log(err.message, "error")
+  return EMPTY
+  // return  throwError(err)
+}
+
   httpOptions = {
     withCredentials: true, headers: {
       'api-Key': '6f873684-0ca2-4d77-8522-60a9b4a13f78'
@@ -36,12 +42,8 @@ export class TodosService {
 
   getTodos() {
     return this.http
-      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists1', this.httpOptions)
-      .pipe(catchError((err: HttpErrorResponse) => {
-          this.beautyLoggerService.log(err.message, "error")
-          return EMPTY
-          // return  throwError(err)
-        })
+      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', this.httpOptions)
+      .pipe(catchError(this.errorHandler.bind(this))
       )
       .subscribe((todos) => {
         this.todos$.next(todos)
@@ -52,11 +54,13 @@ export class TodosService {
   createTodo(title: string) {
     return this.http.post<TodoResponse<{ item: Todo }>>(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {title}, this.httpOptions)
       .pipe(
+        catchError(this.errorHandler.bind(this)),
         map((res) => {
           const stateTodos = this.todos$.getValue()
           const newTodo = res.data.item
           return [newTodo, ...stateTodos]
         })
+
       )
       .subscribe((todos) => {
         this.todos$.next(todos)
@@ -67,6 +71,7 @@ export class TodosService {
   deleteTodo(todoId: string) {
     return this.http.delete<TodoResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, this.httpOptions)
       .pipe(
+        catchError(this.errorHandler.bind(this)),
         map(() => {
           return this.todos$.getValue().filter(td => td.id !== todoId)
         })
