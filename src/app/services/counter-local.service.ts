e7d06ac83e33a03bf88ca65440329cca3f1ae806
Raman <romuch1986@gmail.com>
2023-02-03 10:05:16 +0100

    learn services: @Injectable; service connection

--- src/app/services/counter-local.service.ts
+++ src/app/services/counter-local.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import {BeautyLoggerService} from "./beauty-logger.service";
 
 @Injectable()
 export class CounterLocalService {
+
+  constructor(private beautyLoggerService :BeautyLoggerService) {
+  }
   counter = 0
 
   increase() {
     this.counter ++
+    this.beautyLoggerService.log('increase', 'success')
   }
   decrease () {
     this.counter--
+    this.beautyLoggerService.log('decrease', 'warning')
   }
 }
