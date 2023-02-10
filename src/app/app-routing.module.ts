178743016a47e534286de948c3f6ed402af963a8
Raman <romuch1986@gmail.com>
2023-02-10 11:56:28 +0100

    Learning Routing & Navigation: Registration of routes. Created base configuration.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './routing/about/about.component';
+import {HomeComponent} from "./routing/home/home.component";
+import {PostsComponent} from "./routing/posts/posts.component";
 
-const routes: Routes = [];
+
+//http://localhost:4200/   -> HomeComponent
+//http://localhost:4200/about   -> AboutComponent
+//http://localhost:4200/posts   -> PostsComponent
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'posts', component: PostsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
